99a82bc9ed9840bb5fb09e7009b44237b25dde07
DUDADIDO <82928491+DUDADIDO@users.noreply.github.com>
2024-11-11 01:55:28 +0900

    add file

---
+++ docker/FixerBeApplicationTests.java
@@ -0,0 +1,15 @@
+package FIXER.FIXER_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class FixerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
